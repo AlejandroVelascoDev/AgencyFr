8fce512104b38a97373ad18250456e9dd84aa4cc
Alejandro <dadopelanosvela@gmail.com>
2025-09-27 23:47:33 -0500

    Test

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,6 +1,6 @@
 export default function Contact() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-black p-6">
+    <div className="min-h-screen flex flex-col justify-center items-center bg-black p-6 z-2">
         <h1 className="text-4xl font-bold mb-4">Contact Us</h1>
         <p className="text-lg mb-8 text-center max-w-xl">
             We'd love to hear from you! Whether you have questions, feedback, or just want to say hello, feel free to reach out.
